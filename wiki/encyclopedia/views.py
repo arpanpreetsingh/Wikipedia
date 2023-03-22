e33ccf6c204dcd3936f8759367c17fb447e02745
arpanpreetsingh <arpan4singh@gmail.com>
2023-03-21 21:40:51 -0400

    Entry page working

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-
+import markdown2
 from . import util
+from django.http import HttpResponse
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+# function to open a page for the entry the user searched for 
+
+def entry(request, pk):
+	entry = util.get_entry(pk)
+	if entry == None:
+		return HttpResponse("<p>Entry Not Found</p>")
+	else:
+		html = markdown2.markdown(entry)
+
+		return render(request, "encyclopedia/entry.html", {
+			"title_entry" : pk,
+			"html_content": html
+		})
 
